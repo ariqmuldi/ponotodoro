f995373c9b81d541fad0ebe8bccaee82f5cb6705
Ariq Muldi <ariqmuldi@gmail.com>
2024-09-05 17:41:48 -0700

    Starting code

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+import env from 'dotenv'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const fetchApi = async () => {
+    const response = await axios.get(import.meta.env.VITE_BACKEND_ADDRESS + "api"); 
+    // const response = await axios.get(`${import.meta.env.VITE_BACKEND_ADDRESS}api`);
+    console.log(response.data.fruits);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, [])
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import env from 'dotenv'
+
+const app = express();
+env.config();
+
+const corsOptions = { origin : [process.env.VITE_ADDRESS]} // Port Vite servers run on
+app.use(cors(corsOptions))
+const port = 8080;
+
+app.get("/api", (req, res) => {
+    res.json( {"fruits" : ["apple", "orange", "banana"]} );
+});
+
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+});
